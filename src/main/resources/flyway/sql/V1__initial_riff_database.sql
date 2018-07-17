dffc238e066bb1531cb70d8b712cd4d2b4434385
Dušan Andrić <Dusan.Andric@gmail.com>
2018-07-17 12:18:46 -0400

    adds missing sql content

---
+++ src/main/resources/flyway/sql/V1__initial_riff_database.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE riff (
+    id BIGINT NOT NULL,
+    alias VARCHAR(255),
+    content json,
+    creation_date TIMESTAMP WITHOUT TIME ZONE,
+    shared_publicly BOOLEAN NOT NULL,
+    uid VARCHAR(255),
+    updated_date TIMESTAMP WITHOUT TIME ZONE
+);
