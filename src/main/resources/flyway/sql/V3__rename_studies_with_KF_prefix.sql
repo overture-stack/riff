080e93a6e10afc2fc090570635215cd222022aa7
Alexandre Quessy <alexandre@quessy.net>
2019-06-28 12:32:09 -0400

    Add V3__rename_studies_with_KF_prefix.sql migration

---
+++ src/main/resources/flyway/sql/V3__rename_studies_with_KF_prefix.sql
@@ -0,0 +1,30 @@
+/**
+ * Database migration script for Riff's PostgreSQL database.
+ *
+ * This script renames the study names stored in the queries for the cohort builder and the file repository.
+ * This is done so that the former queries remain valid once the studies have been renamed in the release coordinator.
+ * 
+ * @date 2019-06-28
+ */
+
+/* The search queries in the cohort builder are stored as JSON. */
+UPDATE riff SET content = REPLACE(content::TEXT, '"Pediatric Brain Tumor:', '"Kids First: Pediatric Brain Tumor:')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Ewing Sarcoma:', '"Kids First: Ewing Sarcoma:')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Neuroblastoma Initiation and Progression', '"Kids First: Neuroblastoma Initiation and Progression')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Syndromic Cranial Dysinnervation', '"Kids First: Syndromic Cranial Dysinnervation')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Neuroblastoma Initiation and Progression', '"Kids First: Neuroblastoma Initiation and Progression')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Congenital Diaphragmatic Hernia', '"Kids First: Congenital Diaphragmatic Hernia')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Orofacial Cleft:', '"Kids First: Orofacial Cleft:')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Adolescent Idiopathic Scoliosis', '"Kids First: Adolescent Idiopathic Scoliosis')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '"Enchondromatoses', '"Kids First: Enchondromatoses')::JSON;
+
+/* The search queries in the file repository are stored as their full URL. The SQON is therefore URL-encoded: */
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Pediatric%20Brain%20Tumors%3A%20CBTTC', '%22Kids%20First%3A%20Pediatric%20Brain%20Tumors%3A%20CBTTC')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Syndromic%20Cranial%20Dysinnervation', '%22Kids%20First%3A%20Syndromic%20Cranial%20Dysinnervation')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Neuroblastoma%20Initiation%20and%20Progression', '%22Kids%20First%3A%20Neuroblastoma%20Initiation%20and%20Progression')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Ewing%20Sarcoma%3A%20Genetic%20Risk', '%22Kids%20First%3A%20Ewing%20Sarcoma%3A%20Genetic%20Risk')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Congenital%20Diaphragmatic%20Hernia', '%22Kids%20First%3A%20Congenital%20Diaphragmatic%20Hernia')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Orofacial%20Cleft:%20European%20Ancestry', '%22Kids%20First%3A%20Orofacial%20Cleft:%20European%20Ancestry')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Adolescent%20Idiopathic%20Scoliosis', '%22Kids%20First%3A%20Adolescent%20Idiopathic%20Scoliosis')::JSON;
+UPDATE riff SET content = REPLACE(content::TEXT, '%22Enchondromatoses', '%22Kids%20First%3A%20Enchondromatoses')::JSON;
+
